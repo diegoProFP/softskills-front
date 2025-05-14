2c49247a7950ec806028ffacb435d7fc5db51dfd
diegoProFP <diego.di1@educa.madrid.org>
2025-05-14 22:57:49 +0200

    Ajustes en el wizard de nueva muestra

--- src/app/dashboard/wizard-modal/wizard-modal.component.ts
+++ src/app/dashboard/wizard-modal/wizard-modal.component.ts
@@ -1,20 +1,40 @@
-import { Component, ViewChildren, QueryList, Output, EventEmitter } from '@angular/core';
+import { Component, ViewChildren, QueryList, Output, EventEmitter, OnInit } from '@angular/core';
 import { MatExpansionPanel } from '@angular/material/expansion';
+import { CursoService } from '../../services/curso.service';
+import { Curso } from '../../modelo/curso';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-wizard-modal',
   templateUrl: './wizard-modal.component.html',
   styleUrls: ['./wizard-modal.component.scss']
 })
-export class WizardModalComponent {
-
+export class WizardModalComponent implements OnInit {
   @Output() close = new EventEmitter<void>();
-
   currentStep: number = 1;
   totalSteps: number = 5;
-  isModalVisible: boolean = true;
-
   @ViewChildren(MatExpansionPanel) panels: QueryList<MatExpansionPanel>;
+
+  cursoSeleccionado: Curso | null = null;
+  cursos$: Observable<Curso[]>;
+
+  constructor(private cursoService: CursoService) {}
+
+  ngOnInit() {
+    this.cursoSeleccionado = this.cursoService['cursoSeleccionado'];
+    if (!this.cursoSeleccionado) {
+      this.cursos$ = this.cursoService.getCursos();
+    } else {
+      this.currentStep = 2; // Establece el paso inicial en el segundo panel
+    }
+  }
+
+  ngAfterViewInit() {
+    if (this.currentStep === 2) {
+      const panelArray = this.panels.toArray();
+      panelArray[1].open(); // Abre el segundo panel
+    }
+  }
 
   nextStep() {
     if (this.currentStep < this.totalSteps) {
@@ -27,9 +47,9 @@ export class WizardModalComponent {
   previousStep() {
     if (this.currentStep > 1) {
       const panelArray = this.panels.toArray();
-      panelArray[this.currentStep - 1].close(); // Cierra el panel actual
+      panelArray[this.currentStep - 1].close();
       this.currentStep--;
-      panelArray[this.currentStep - 1].open(); // Abre el panel anterior
+      panelArray[this.currentStep - 1].open();
     }
   }
 
--- src/app/services/curso.service.ts
+++ src/app/services/curso.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Curso } from '../modelo/curso';
+import { tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+import { Curso } from '../modelo/curso';
 
-
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class CursoService {
   private rootApiUrl = environment.apiUrl;
-
   private apiUrl = this.rootApiUrl + '/cursos';
+  private cursoSeleccionado: Curso;
 
   constructor(private http: HttpClient) { }
 
   getCursos(): Observable<Curso[]> {
-    return this.http.get<Curso[]>(this.apiUrl);
+    return this.http.get<Curso[]>(this.apiUrl).pipe(
+      tap(() => this.cursoSeleccionado = null)
+    );
   }
 
-  getCursoById(id: string) {
-    return this.http.get<Curso>(`${this.apiUrl}/${id}`);
+  getCursoById(id: string): Observable<Curso> {
+    return this.http.get<Curso>(`${this.apiUrl}/${id}`).pipe(
+      tap(curso => this.cursoSeleccionado = curso)
+    );
   }
 }
